7f2f86ee0cacaab3208717d494f28709b92f900b
Richard P Sharp Jr <richpsharp@gmail.com>
2024-09-17 14:06:09 -0700

    add dependent task and remove debugging

--- biomass_cci/biomass_cci_README.txt
+++ biomass_cci/biomass_cci_README.txt
@@ -1,14 +1,7 @@
-Data all downlaoded from
+1) Aboveground biomass data downloaded from `https://data.ceda.ac.uk/neodc/esacci/biomass/data/agb/maps/v5.0/geotiff` with this command: `echo https://dap.ceda.ac.uk/neodc/esacci/biomass/data/agb/maps/v5.0/geotiff/{2010/,2015/,2016/,2017/,2018/,2019/,2020/,2021/} | \
+  tr ' ' '\n' | xargs -n 1 -P 8 wget -e robots=off --mirror --no-parent -r`
+ * Note: Units of these raster are biomass metric tons per hectare
 
-https://data.ceda.ac.uk/neodc/esacci/biomass/data/agb/maps/v5.0/geotiff
+2) Rasters then clipped by year to `Pilot_scapes` and projected into `EPSG:8857` (equal world area projection)
 
-with this command
-
-echo https://dap.ceda.ac.uk/neodc/esacci/biomass/data/agb/maps/v5.0/geotiff/{2010/,2015/,2016/,2017/,2018/,2019/,2020/,2021/} | \
-  tr ' ' '\n' | xargs -n 1 -P 8 wget -e robots=off --mirror --no-parent -r
-
-Rasters then clipped by year to `Pilot_scapes` and projected into `EPSG:8857`
-
-
-
-Project to EPSG:8857 (equal world area projection)
+3) Total biomass is calculated as (sum of clipped biomass * area of valid pixels in hectares)
--- biomass_cci/process_biomass_cci.py
+++ biomass_cci/process_biomass_cci.py
@@ -3,19 +3,14 @@ from collections import defaultdict
 import glob
 import logging
 import os
-import shutil
 
 import numpy as np
 from ecoshard import taskgraph
 from osgeo import gdal
 from osgeo import osr
 from osgeo import ogr
-import ecoshard
 import numpy
-
 import ecoshard.geoprocessing as geoprocessing
-import ecoshard.geoprocessing.routing as routing
-from ecoshard import taskgraph
 
 
 gdal.SetCacheMax(2**27)
@@ -26,26 +21,28 @@ logging.basicConfig(
         '%(asctime)s (%(relativeCreated)d) %(processName)s %(levelname)s '
         '%(name)s [%(funcName)s:%(lineno)d] %(message)s'))
 LOGGER = logging.getLogger(__name__)
-logging.getLogger('taskgraph').setLevel(logging.WARN)
+logging.getLogger('ecoshard.taskgraph').setLevel(logging.WARN)
 logging.getLogger('ecoshard.geoprocessing').setLevel(logging.WARN)
 
 CLIPPED_DIR = 'clipped_rasters'
 
 TARGET_PIXEL_SIZE = (90, -90)  # I know those rasters are 90m
+NODATA = 65535
 
 
 def sum_biomass(biomass_raster):
+    nodata = geoprocessing.get_raster_info(biomass_raster)['nodata'][0]
     biomass_array = gdal.OpenEx(biomass_raster).ReadAsArray().astype(float)
-    biomass_sum = np.sum(biomass_array)
-    valid_pixel_count = np.sum(biomass_array >= 0)
-    biomass_array = None
-
+    valid_mask = biomass_array != nodata
+    biomass_sum = np.sum(biomass_array[valid_mask])
+    valid_pixel_count = float(np.sum(valid_mask))
     # raster units are t / hectare
     total_biomass_sum = (
         biomass_sum *
         valid_pixel_count *
         abs(numpy.multiply(*TARGET_PIXEL_SIZE)) / 1e4)
 
+    biomass_array = None
     return total_biomass_sum
 
 
@@ -71,11 +68,11 @@ def warp_raster_to_new_projection(
         vector_mask_options={
             'mask_vector_path': clipping_vector_path,
             'mask_vector_where_filter': f'id={clipping_vector_fid}',
-            'target_mask_value': -1},
+            'target_mask_value': NODATA},
         gdal_warp_options=None, working_dir='.')
     target_raster = gdal.OpenEx(target_raster_path, gdal.GA_Update)
-    target_band = target_raster.GetBand(1)
-    target_band.SetNoDataValue(-1)
+    target_band = target_raster.GetRasterBand(1)
+    target_band.SetNoDataValue(NODATA)
     target_band = None
     target_raster = None
 
@@ -107,13 +104,12 @@ def add_biomass_sum_to_features(
         output_feature.SetFrom(feature)
         for year, biomass_sum_by_feature_name in biomass_sum_lookup_by_year.items():
             analysis_id = f'biomass_t_sum_{year}'
-            feature_name = feature.GetField('Scape')  # Assuming 'Scape' field contains feature name
+            feature_name = feature.GetField('Scape')
             biomass_sum = biomass_sum_by_feature_name[feature_name]
             output_feature.SetField(str(analysis_id), float(biomass_sum.get()))
         output_layer.CreateFeature(output_feature)
         output_feature = None
 
-    # Clean up
     vector = None
     output_vector = None
 
@@ -126,7 +122,7 @@ def main():
     epsg_8857 = osr.SpatialReference()
     epsg_8857.ImportFromEPSG(8857)
     epsg_8857_wkt = epsg_8857.ExportToWkt()
-    task_graph = taskgraph.TaskGraph('.', n_workers=-1) #os.cpu_count())
+    task_graph = taskgraph.TaskGraph('.', n_workers=os.cpu_count())
     target_pixel_size = (90, -90)  # I know the base data are 90m pixels
     vector_path = "./Biomass_cci_geotiffs/Pilot_scapes"
     vector = gdal.OpenEx(vector_path, gdal.OF_VECTOR)
@@ -143,14 +139,10 @@ def main():
                 feature_bounding_box[i] for i in [0, 2, 1, 3]]
             fid = feature.GetFID()
             feature_name = feature.GetField('Scape')
-            print(fid)
-            print(feature_bounding_box)
-
             target_raster_path = os.path.join(
                 CLIPPED_DIR,
                 f'biomass_cci_{os.path.basename(os.path.splitext(raster_path)[0])}_'
                 f'{feature_name}.tif').replace(' ', '_')
-            print(target_raster_path)
             warp_task = task_graph.add_task(
                 func=warp_raster_to_new_projection,
                 args=(
@@ -167,6 +159,7 @@ def main():
                 func=sum_biomass,
                 args=(target_raster_path,),
                 store_result=True,
+                dependent_task_list=[warp_task],
                 task_name=f'sum up {target_raster_path}')
             biomass_sum_lookup_by_year[year][feature_name] = sum_task
 
