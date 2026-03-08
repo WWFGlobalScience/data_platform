815d3d9e5a12e869a3f64f82925ac98067e846dd
Samantha Cheng <cheng@nceas.ucsb.edu>
2026-03-08 12:02:02 -0700

    Add files via upload
    Updated aesthetics for plotting in grand corruption (to be implemented as a well across other indicators). Updated scape list and subnational area overlap using new logic (25% of the scape OR 95% of the subnational area in the scape).

--- Governance/Subnational_Grand_Corruption.R
+++ Governance/Subnational_Grand_Corruption.R
@@ -1,9 +1,7 @@
 # ------------------------------------------------------------------------------
-# Subnational grand Corruption Trends— Baseline (GDL, governance dataset)
-# https://globaldatalab.org/asset/476/GDL-CorruptionData-1.0.csv
-# The raw data links require a login to access
+# Subnational Grand Corruption Trends— Baseline (GDL, governance dataset)
 # Outputs:
-#   1) GeoPackage of overlap polygons with SCI columns (grand_YYYY)
+#   1) GeoPackage of overlap polygons with grand corruption columns (grand_YYYY)
 #   2) One figure per scape: grand corruption trends for overlapping subnational areas
 # ------------------------------------------------------------------------------
 
@@ -19,8 +17,7 @@ library(scales)
 # Load GDL subnational areas that overlap with landscapes (at least 25% of scape)
 # ------------------------------------------------------------------------------
 
-gdl_scape_overlap_25 <- read_rds("data_inputs/gdl_scape_overlap_25.rds") %>%
-  select(-region)
+gdl_scape_overlap_retained <- readRDS("data_inputs/gdl_scape_overlap_retained.rds")
 
 # ------------------------------------------------------------------------------
 # Load GDL subnational corruption data (SCI)
@@ -49,30 +46,51 @@ grand_wide <- grand %>%
 # ------------------------------------------------------------------------------
 # Join overlap polygons with petty table
 # ------------------------------------------------------------------------------
-gdl_join <- gdl_scape_overlap_25 %>%
-  left_join(grand_wide, by = "gdlcode")
+gdl_join <- gdl_scape_overlap_retained %>%
+  left_join(select(grand_wide,-region), by = "gdlcode")
 
 # ------------------------------------------------------------------------------
 # Write output (25% only)
 # ------------------------------------------------------------------------------
 run_date <- format(Sys.Date(), "%Y_%m_%d")
-gdl_prod_25_out <- gdl_join %>%
-  select(-region,-scape_name,-country)
+gdl_prod_out <- gdl_join %>%
+  rename(scape_id = ID)
 
-filename <- paste0("Governance/subnat_corruption_grand_25_pct_", run_date, ".csv")
+filename <- paste0("Governance/subnat_corruption_grand_", run_date, ".csv")
 
-write.csv(gdl_prod_25_out,paste0(filename))
+write.csv(gdl_prod_out,paste0(filename))
+
+# ------------------------------------------------------------------------------
+# Write output to GeoPackage
+# ------------------------------------------------------------------------------#
+# Read in the overlapped areas from GPKG
+gdl_scapes_overlap <- st_read(
+  "data_inputs/gdl_scape_overlap_retained.gpkg",
+  quiet = TRUE
+) %>%
+  st_make_valid()
+
+gdl_scapes_overlap <- gdl_scapes_overlap %>% select(gdlcode,ID,geom)
+
+gdl_prod_out2 <- gdl_scapes_overlap %>%
+  left_join(gdl_join, by = c("gdlcode", "ID")) %>%
+  rename(scape_id = ID)
+
+gdl_prod_out2 <- st_transform(gdl_prod_out2, 4326)
+
+filename2 <- paste0("Governance/subnat_corruption_grand_", run_date, ".gpkg")
+
+st_write(gdl_prod_out2, paste0(filename2), layer = "subnational_grand_corruption", delete_layer = TRUE)
 
 # ------------------------------------------------------------------------------
 # Figures:grand trends within each scape (lines = overlapping GDL subnational areas)
 # ------------------------------------------------------------------------------
-fig_dir <- "Scapes_Grand_Corruption_Trends"
+fig_dir <- "Scapes_Grand_Corruption_Trends/03-06-2026"
 dir.create(fig_dir, recursive = TRUE, showWarnings = FALSE)
 
-grand_long_scape <- gdl_out %>%
-  st_drop_geometry() %>%
+grand_long_scape <- gdl_prod_out %>%
   distinct(
-    scape_id, scape_name, gdlcode, region.x,
+    scape_id, scape_name, gdlcode, region,
     across(starts_with("grand_")),
     .keep_all = TRUE
   ) %>%
@@ -84,11 +102,20 @@ grand_long_scape <- gdl_out %>%
   mutate(
     year = as.integer(str_extract(year, "\\d{4}")),
     corruption_index = as.numeric(corruption_index),
-    gdl_name = str_squish(region.x)
-  )
+    gdl_name = str_squish(region)
+  ) %>%
+  filter(!is.na(corruption_index))
 
 scape_list <- sort(unique(grand_long_scape$scape_id))
 
+##Define palette based on the maximum number of subnational areas per scape
+nb.cols <- max(tally(group_by(distinct(select(grand_long_scape,gdlcode,scape_id)),scape_id))$n)
+
+#Extend an existing palette (e.g., Set3) to that number
+library(RColorBrewer)
+mycolors <- colorRampPalette(brewer.pal(8, "Set3"))(nb.cols)
+
+#Plot all scapes
 for (sid in scape_list) {
   
   df_sub <- grand_long_scape %>%
@@ -106,7 +133,6 @@ for (sid in scape_list) {
     fig_dir,
     paste0("Grand_Corruption_GDL_Trends_Scape_", sid, "_", safe_scape, ".png")
   )
-  
   
   df_line <- df_sub %>%
     filter(!is.na(corruption_index)) %>%
@@ -133,19 +159,21 @@ for (sid in scape_list) {
     ) +
     labs(
       title = paste("Subnational Grand Corruption Trends in", scape_nm),
-      subtitle = "Subnational areas overlapping with at least 25% of operational landscape",
+      subtitle = "Subnational areas overlapping with the operational landscape",
       x = "Year",
       y = "Index value",
       color = "Subnational areas",
       linetype = "Subnational areas",
       caption = "Source: Global Data Lab (GDL), governance dataset. Indicator: Grand Corruption Index."
     ) +
-    scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
+    scale_x_continuous(breaks = seq(1995, 2022, by = 1)) +
+    scale_colour_manual(values = mycolors) +
     theme_minimal(base_size = 13) +
     theme(
       plot.title    = element_text(size = 12, face = "bold", hjust = 0.5),
       plot.subtitle = element_text(size = 8, hjust = 0.5),
       axis.text     = element_text(color = "black"),
+      axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
       axis.line     = element_line(color = "black"),
       legend.position = "right",
       legend.title  = element_text(size = 11),
